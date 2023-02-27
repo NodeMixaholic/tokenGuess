44102cc97a856d547e01ea763d6cb02dc92887a3
NodeMixaholic <sam@sllord.info>
2023-02-27 17:02:30 -0600

    Create guesser.js

---
+++ guesser.js
@@ -0,0 +1,18 @@
+function guessToken(id) {
+    let length = 27
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return btoa(id) + "." + btoa(Math.floor(Date.now() / 1000) + 1293840000) + "." + guessHMAC();
+}
+
+let id = prompt("Please enter a person's numeric discord ID")
+while (true) {
+console.log(guessToken(id))
+await new Promise(r => setTimeout(r, 2000));
+}
